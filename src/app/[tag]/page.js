7ee9131eada9431b8f7550e867c7e95dde2d8fa5
sundeep <slowlymakesyouperfect@gmail.com>
2023-12-12 12:31:46 +0530

    ok

--- src/app/[tag]/page.js
+++ src/app/[tag]/page.js
@@ -5,7 +5,8 @@ import Load from "../components/loader"
 import Script from 'next/script'
 export function generateMetadata({params}){
     return{
-        title:params.tag+"-My Code Helper"
+        title:params.tag+" - My Code Helper",
+        description:params.tag+ " find here code examples, projects, interview questions, cheatsheet, and problem solution you have needed. Many source codes of "+params.tag+" are available for free here."
     }
 }
 export default function tag(params){
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
-import Script from 'next/script'
+// import Script from 'next/script'
 export const metadata = {
   title: 'My Code Helper',
   description: 'My code helper is a web app created using next js to get working solutions of code any time.',
@@ -11,9 +11,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Script async="async" data-cfasync="false" src="//collectbladders.com/22117bd38cb131c1c5659583265cdd1d/invoke.js"></Script>
+      {/* <Script async="async" data-cfasync="false" src="//collectbladders.com/22117bd38cb131c1c5659583265cdd1d/invoke.js"></Script> */}
         {children}
-        <div id="container-22117bd38cb131c1c5659583265cdd1d"></div>
+        {/* <div id="container-22117bd38cb131c1c5659583265cdd1d"></div> */}
         </body>
     </html>
   )
