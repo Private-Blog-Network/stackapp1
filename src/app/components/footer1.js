80bf97d13ce20a4a7243d9bc274e546c239aacee
sundeep <slowlymakesyouperfect@gmail.com>
2024-01-08 00:33:06 +0530

    lets watch

--- src/app/components/footer1.js
+++ src/app/components/footer1.js
@@ -1,12 +1,12 @@
 export default function Footer1(){
     return(
-        <><hr />
+        <div className="container"><hr />
         <footer className="bg-body-tertiary text-center text-lg-start">
   <div className="text-center p-3">
     Content Source : 
     <a className="text-body text-primary" href="https://stackoverflow.com" rel="nofollow noopener" id="ftlk">Stackoverflow</a>
   </div>
 </footer>
-        </>
+        </div>
     )
 }
