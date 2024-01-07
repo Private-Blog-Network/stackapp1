344afdc9cc3cce4de2d99b2c1b7c027db96f1eb8
sundeep <slowlymakesyouperfect@gmail.com>
2024-01-08 00:04:01 +0530

    lets watch

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 import Card from "./components/card"
 import Nav from "./components/nav"
 import Script from "next/script"
-import Footer1 from "./components/footer1"
+import Footer from "./components/footer"
 import Load from "./components/loader"
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 export default function Home() {
   return (
     <div>
       <Nav/>
       <Load/>
       <Card name="home"/>
-      <Footer1/>
+      <Footer/>
     </div>
   )
 }
