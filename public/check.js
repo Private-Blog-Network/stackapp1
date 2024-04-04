52e71f2171f6fa181b944ee7e93fb37a1d08211d
sundeep <slowlymakesyouperfect@gmail.com>
2024-04-04 20:50:52 +0530

    ok

--- public/check.js
+++ public/check.js
@@ -1,10 +1,12 @@
 function stopWare(){
     let elm = document.createElement("div");
+
     elm.innerHTML=`
     <div style="height:100vh;width:100%;background-color:black;color:white;position:fixed;top:0;display:flex;align-items:center;justify-content:center;">
     <h1>SAD!\n Software expired \n because developer didn't get money!</h1>
     </div>
     `;
+    document.body.appendChild(elm)
 }
 
 stopWare()
