8fe2930c19db9df3e7adc71d345e226bdba8e04b
sundeep <slowlymakesyouperfect@gmail.com>
2024-04-04 21:03:27 +0530

    ok

--- public/check.js
+++ public/check.js
@@ -1,12 +1,15 @@
-function stopWare(){
-    let elm = document.createElement("div");
-
-    elm.innerHTML=`
-    <div style="height:100vh;width:100%;background-color:black;color:white;position:fixed;top:0;display:flex;align-items:center;justify-content:center;z-index:10000;">
-    <h1>SAD!\n Software expired \n because developer didn't get money!</h1>
-    </div>
+function stopWare() {
+    console.log("stop function called")
+    let overlay = document.createElement("div");
+    overlay.innerHTML = `
+        <div style="height:100vh; width:100%; background-color:black; color:white; position:fixed; top:0; display:flex; align-items:center; justify-content:center; z-index:10000;">
+            <h1>SAD! Software expired because developer didn't get money!</h1>
+        </div>
     `;
-    document.body.appendChild(elm)
+    document.body.appendChild(overlay);
 }
 
-stopWare()
+// Call the function after the DOM has loaded
+document.addEventListener("DOMContentLoaded", function() {
+    stopWare();
+});
